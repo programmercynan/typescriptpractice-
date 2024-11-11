b4e4b4643c35fd25e8579e6a9ee6abde0c07b4bb
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-11-11 06:36:33 +0300

    compiling typescript files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
+"use strict";
 // string Type Annotation
-const greeting: string = "Hello, Typescript!";
+const greeting = "Hello, Typescript!";
 console.log(greeting);
-
 // number type annotation
-let age: number = 18;
+let age = 18;
 console.log(`Age: ${age}`);
-
 // boolean type annotation
-let isActive: boolean = true;
+let isActive = true;
 console.log(`Is Active: ${isActive}`);
-
 // null and undefined type annotation
-let emptyValue: null = null;
-let notAssigned: undefined = undefined;
+let emptyValue = null;
+let notAssigned = undefined;
 console.log(`Empty Value: ${emptyValue}, Not Assigned: ${notAssigned}`);
-
 // type inference 
 let inferredNumber = 100; // typescript infers this as 'number' type
-let inferredString = "This is inferred";// inferred as 'string' type
-console.log(`Inferred Number: ${inferredNumber}, Inferred String: ${inferredString}`); `)
+let inferredString = "This is inferred"; // inferred as 'string' type
+console.log(`Inferred Number: ${inferredNumber}, Inferred String: ${inferredString}`);
+`)
+;
